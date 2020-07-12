c2981fa0089abfc49fe3ee9f9a82dcc8436ef192
Marwa <marwa1995393@gmail.com>
2020-07-12 15:06:48 +0200

    [add] add invoice_report_update

--- invoice_reports/__manifest__.py
+++ invoice_reports/__manifest__.py
@@ -17,7 +17,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account_accountant'],
+    'depends': ['base','account_accountant','company_fields'],
 
     # always loaded
     'data': [

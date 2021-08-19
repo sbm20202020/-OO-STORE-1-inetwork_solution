fd06521ae46e0502ccb432334c92375595dae516
Marwa Ahmed <marwa1995393@gmail.com>
2021-08-19 14:21:01 +0200

    [Add]

---
+++ attendance_device_edition/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Attendance Customization',
+    'version': '13',
+    'category': 'Attendance',
+    'summary': """
+    """,
+    'author': 'ITSYSTEMS Ebtsam',
+    'depends': ['to_attendance_device'],
+    'data': [
+        'views/attendance_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}

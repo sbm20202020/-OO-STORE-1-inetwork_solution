d242e36a15e44e68bdab050cc5281a776526848a
radwa21 <radwa.abdelkader.1@gmail.com>
2022-02-27 13:38:55 +0200

    []

--- eta_invoice/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import eta_invoice
-from . import account_invoice_line
-from . import account_tax
-from . import product_product
-from . import res_company
-from . import res_partner
-from . import uom_uom
